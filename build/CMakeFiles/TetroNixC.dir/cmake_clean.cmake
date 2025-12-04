2f72dec63df6be613c0c5f6f828d74437efdb411
rob0xf <dead.0x0f@gmail.com>
2025-12-03 20:21:05 -0600

    Starting development environment

---
+++ build/CMakeFiles/TetroNixC.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TetroNixC.dir/link.d"
+  "CMakeFiles/TetroNixC.dir/src/main.c.o"
+  "CMakeFiles/TetroNixC.dir/src/main.c.o.d"
+  "TetroNixC"
+  "TetroNixC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TetroNixC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/TetroNixC.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TetroNixC.
+# This may be replaced when dependencies are built.
